b1489cb449f9ab208ce2415a2db9e2d6e4a93a54
FindoKampete <kampete_findo@batazia.com>
2024-10-10 00:33:31 +0100

    details and started lauth

--- src/navigation/stacks/HomeStackNavigator.tsx
+++ src/navigation/stacks/HomeStackNavigator.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { HomeStackParamList } from '../types'
 import Home from '../../screens/Home/Home.screen';
+import { Details } from 'src/screens';
 
 const Stack = createNativeStackNavigator<HomeStackParamList>();
 
 const HomeStackNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Details'>
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   )
 }
--- src/screens/Home/Home.screen.tsx
+++ src/screens/Home/Home.screen.tsx
@@ -1,35 +1,37 @@
-import { FlatList, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
 
 // Icons
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Feather from "@expo/vector-icons/Feather";
 
-import styles from './Home.style'
+import styles from "./Home.style";
 
-import { PRIMARY_WHITE_COLOR } from '../../utils/colors';
-import { AntDesign } from '@expo/vector-icons';
-import { DEVICE_HEIGHT, DEVICE_WIDTH } from '../../utils/sizes';
-import { CoinType } from '../../types';
-import { TrendingCryptoItem } from '@components/index';
-import { useGetCryptoMarketsQuery } from 'src/redux/reducers/coinSlice';
-import { useGetTrendingSearchQuery } from 'src/redux/reducers/trendingSearchSlice';
+import { PRIMARY_WHITE_COLOR } from "../../utils/colors";
+import { AntDesign } from "@expo/vector-icons";
+import { DEVICE_HEIGHT, DEVICE_WIDTH } from "../../utils/sizes";
+import { CoinType } from "../../types";
+import { TrendingCryptoItem } from "@components/index";
+import { useGetCryptoMarketsQuery } from "src/redux/reducers/coinSlice";
+import { useGetTrendingSearchQuery } from "src/redux/reducers/trendingSearchSlice";
 
 const Home = () => {
+  const [page, setPage] = useState<number>(1);
 
-  const [page, setPage] = useState<number>(1)
+  const { data, error, isLoading } = useGetCryptoMarketsQuery({ page });
+  const {
+    data: trendingSearchData,
+    error: trendingSearchError,
+    isLoading: loadingTrendingSearch,
+  } = useGetTrendingSearchQuery(undefined);
 
-  const { data, error, isLoading } = useGetCryptoMarketsQuery({page});
-  const { data: trendingSearchData, error: trendingSearchError, isLoading: loadingTrendingSearch } = useGetTrendingSearchQuery({})
+  // console.log("Trending markets", trendingSearchData);
 
-  console.log("Trending markets", trendingSearchData);
-  
-  const renderTrendingCoin = ({ item }:{item:CoinType}) => {
-    return (
-      <TrendingCryptoItem coin={item} />
-    )
-  }
+  const renderTrendingCoin = ({ item }: { item: CoinType }) => {
+    // console.log('item', item)
+    return <TrendingCryptoItem coin={item} />;
+  };
 
   return (
     <View style={styles.container}>
@@ -46,27 +48,45 @@ const Home = () => {
       <View style={styles.sendButtonRecieve}>
         <View style={styles.iconAndText}>
           <View style={styles.iconContanier}>
-            <Feather name="arrow-up-right" size={24} color={PRIMARY_WHITE_COLOR} />
+            <Feather
+              name="arrow-up-right"
+              size={24}
+              color={PRIMARY_WHITE_COLOR}
+            />
           </View>
           <Text style={styles.sendText}>Send</Text>
         </View>
         <View style={styles.iconAndText}>
-          <View style={[styles.iconContanier, { backgroundColor: '#315BF0', borderWidth: 0 }]}>
-            <AntDesign name="plus" size={24} color={PRIMARY_WHITE_COLOR} fas/>
+          <View
+            style={[
+              styles.iconContanier,
+              { backgroundColor: "#315BF0", borderWidth: 0 },
+            ]}
+          >
+            <AntDesign name="plus" size={24} color={PRIMARY_WHITE_COLOR} fas />
           </View>
           <Text style={styles.buyText}>Buy</Text>
         </View>
         <View style={styles.iconAndText}>
           <View style={styles.iconContanier}>
-            <Feather name="arrow-down-left" size={24} color={PRIMARY_WHITE_COLOR} />
+            <Feather
+              name="arrow-down-left"
+              size={24}
+              color={PRIMARY_WHITE_COLOR}
+            />
           </View>
           <Text style={styles.receiveText}>Receive</Text>
         </View>
       </View>
-      <View style={{ paddingLeft: DEVICE_WIDTH * 0.05, marginTop: DEVICE_HEIGHT * 0.05  }}>
+      <View
+        style={{
+          paddingLeft: DEVICE_WIDTH * 0.05,
+          marginTop: DEVICE_HEIGHT * 0.05,
+        }}
+      >
         <Text style={styles.trendingText}>Trending coins</Text>
         <FlatList
-          data={trendingSearchData?.coins.map(coin=>coin.item)}
+          data={trendingSearchData?.coins.map((coin) => coin.item)}
           renderItem={renderTrendingCoin}
           horizontal
           style={styles.trendingFlatlist}
@@ -77,7 +97,7 @@ const Home = () => {
         />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
